d43434552c4f38bc2ff5fb35f36be9e0c5fa7506
Seth Wright <seth@crosse.org>
2011-09-28 11:14:54 -0400

    structural change, plus initial versioning of a few files.  This commit will be cleaned up in later commits.

--- notprocessed/Get-ActiveSyncStats.psm1
+++
@@ -1,6 +0,0 @@
-$users = Get-CASMailbox -ResultSize Unlimited -Filter { HasActiveSyncDevicePartnership -eq $true -and -not DisplayName -like "CAS_{*" } | Get-Mailbox -ResultSize Unlimited
-
-foreach ($user in $users) { 
-    Get-ActiveSyncDeviceStatistics -Mailbox $user -ErrorAction SilentlyContinue 
-} | Select Identity,FirstSyncTime,LastSyncSuccess,LastSyncAttemptTime,Devicetype,DeviceModel,DeviceUserAgent,DevicePhoneNumber
-
